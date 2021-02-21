f6ad07da3acec5eba022ebce29be08c12737e777
rgarsan <rgarsan84@gmail.com>
2021-02-21 22:50:50 +0100

    cars-JSON

---
+++ cars.js
@@ -0,0 +1,103 @@
+let cars = [
+
+    {
+brand:'Aston Martin',
+model:'Superleggera',
+topSpeed: 340,
+maxPower:725,
+engineSize:5200,
+image:'https://cdn.drivek.it/configurator-covermobile/cars/es/$original$/ASTON-MARTIN/DBS-SUPERLEGGERA/38826_COUPE-2-DOORS/aston-martin-dbs-superleggera-2018-cover-mobile.jpg'
+},
+
+{
+brand:'Mercedes',
+model:'AMG GT',
+topSpeed: 310,
+maxPower:510,
+engineSize:4000,
+image:'https://www.coches.com/fotos_historicas/amg/Mercedes-GT-2015/amg_mercedes-gt-2015_r45.jpg'
+},
+
+{
+brand:'Ferrari',
+model:'SF90 Spider',
+topSpeed: 340,
+maxPower:1000,
+engineSize:4000,
+image:'https://noticias.coches.com/wp-content/uploads/2020/11/Ferrari-SF90-Spider-2021-3.jpeg'
+},
+
+{
+brand:'Bugatti',
+model:'Divo',
+topSpeed:380 ,
+maxPower:1500,
+engineSize:8000,
+image:'https://noticias.coches.com/wp-content/uploads/2018/08/bugatti-divo-3.jpg'
+},
+
+{
+brand:'Lamborghini',
+model:'Huracan',
+topSpeed: 325,
+maxPower:640,
+engineSize:5200,
+image:'https://noticias.coches.com/wp-content/uploads/2019/01/lamborghini-huracan-evo-2019-4.jpg'
+},
+
+{
+brand:'Audi',
+model:'R8',
+topSpeed: 331,
+maxPower:620,
+engineSize:5200,
+image:'https://noticias.coches.com/wp-content/uploads/2018/10/Audi-R8-2019-8.jpg'
+},
+
+{
+brand:'Porche',
+model:'Targa',
+topSpeed: 289,
+maxPower:385,
+engineSize:3000,
+image:'https://noticias.coches.com/wp-content/uploads/2020/05/Porsche-911-Targa-2020-2.jpeg'
+},
+
+{
+brand:'Bentley',
+model:'Continental GT',
+topSpeed:280 ,
+maxPower:529,
+engineSize:4000,
+image:'https://noticias.coches.com/wp-content/uploads/2020/04/Bentley-Continental-GT-Convertible-2012-13.jpg'
+},
+
+{
+brand:'BMW',
+model:'650',
+topSpeed:250 ,
+maxPower:450,
+engineSize:3500,
+image:'https://noticias.coches.com/wp-content/uploads/2014/12/BMW-Serie-6-Gran-Coupe-2014-11-650x433.jpg'
+},
+
+{
+brand:'Maserati',
+model:'Gran Turismo',
+topSpeed:301,
+maxPower:460,
+engineSize:4700,
+image:'https://noticias.coches.com/wp-content/uploads/2017/06/maserati-granturismo-2018.jpg'
+},
+
+{
+brand:'Aston Martin',
+model:'Vanquish',
+topSpeed:295 ,
+maxPower:565,
+engineSize:5935,
+image:'https://noticias.coches.com/wp-content/uploads/2012/06/Aston_Martin_Vanquish_01.jpg'
+}
+]
+
+
