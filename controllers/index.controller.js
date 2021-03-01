6616b5608d253dff3c21533747437ef10f502be9
Eduardo García <edugarciasb16@gmail.com>
2021-03-01 21:40:18 +0100

    Carrousel hbs

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,5 +1,11 @@
-//Models
+const Car = require('../models/car.model')
 
 module.exports.home = (req, res, next) => {
-    res.render('index')
+    Car.find({})
+        .then((cars) => {
+            console.log(cars)
+
+            res.render('index', {cars:cars.slice(0,3)})
+        })
+        .catch((e) => next(e))
 }
