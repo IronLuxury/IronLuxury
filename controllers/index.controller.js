be97185d52f20576e799a32a3db102d9d02d2c71
Eduardo García <edugarciasb16@gmail.com>
2021-03-03 01:12:42 +0100

    remove logs

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -3,7 +3,6 @@ const Car = require('../models/car.model')
 module.exports.home = (req, res, next) => {
     Car.find({})
         .then((cars) => {
-            console.log(cars)
             res.render('index', {cars:cars.slice(0,3)})
         })
         .catch((e) => next(e))
