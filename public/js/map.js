bc30fafaf8ab15ec6741aa1429d29fdab13f0b08
rgarsan <rgarsan84@gmail.com>
2021-03-10 21:20:33 +0100

    googlemaps done

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,13 @@
-let map
-
 function initMap() {
-    
+    const defaultPosition = { lat: 40.33485055409575, lng:  -3.7048817687377777 }
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 14,
-      center:  { lat: 40.4167278, lng: -3.7033387 }
+      center: defaultPosition,
     });
+
+    const marker = new google.maps.Marker({
+        position:defaultPosition,
+        map: map
+    })
+
 }
